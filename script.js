9a82e7b709fca0fd32014dcf6203c89a2a4cc2db
nxnikolaos <nxnatlas@gmail.com>
2022-03-03 12:44:17 +0200

    Class added on each square div

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const gridDiv = [];
 for(let i=0; i<16; i++){
     for(let j=0; j<16; j++){
         gridDiv[i,j] = document.createElement('div');
+        gridDiv[i,j].classList.add('grid-square');
         parent.appendChild(gridDiv[i,j]);
     }
 }
