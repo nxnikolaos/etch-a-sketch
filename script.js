03cdfdd8ef4accd86134f267e0816f9668a5b768
nxnikolaos <nxnatlas@gmail.com>
2022-03-03 16:18:18 +0200

    Completed mouseenter trail feature

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ for(let i=0; i<16; i++){
     for(let j=0; j<16; j++){
         gridDiv[i,j] = document.createElement('div');
         gridDiv[i,j].classList.add('grid-square');
+        gridDiv[i,j].addEventListener("mouseenter",function(){
+            this.style.backgroundColor = '#e900ff';
+        })
         parent.appendChild(gridDiv[i,j]);
     }
 }
+
